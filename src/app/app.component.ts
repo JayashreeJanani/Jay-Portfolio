a55bf926991d9addb51fb02be9fd23c36fd126c5
Jayashree Janani <jayashree.199731@gmail.com>
2026-08-09 23:29:56 -0300

    hero page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import {HeroComponent} from "./components/hero/hero.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent,  NavbarComponent],
+  imports: [HeroComponent, NavbarComponent,  NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
