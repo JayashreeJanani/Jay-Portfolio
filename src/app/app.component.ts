f79f31e4c4760097e7fedaa56f231984eca339dd
Jayashree Janani <jayashree.199731@gmail.com>
2026-08-12 18:20:21 -0300

    Experience component built

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeroComponent } from "./components/hero/hero.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroComponent, NavbarComponent, AboutMeComponent],
+  imports: [HeroComponent, NavbarComponent, AboutMeComponent, ExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
