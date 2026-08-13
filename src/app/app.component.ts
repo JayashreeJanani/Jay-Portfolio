ba05b43411f195c23805632c259553e399c99327
Jayashree Janani <jayashree.199731@gmail.com>
2026-08-13 11:02:15 -0300

    Completed modules 1.projects 2.skills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { HeroComponent } from "./components/hero/hero.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { ExperienceComponent } from './components/experience/experience.component';
+import {ProjectsComponent} from "./components/projects/projects.component";
+import {SkillsComponent} from "./components/skills/skills.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroComponent, NavbarComponent, AboutMeComponent, ExperienceComponent],
+  imports: [HeroComponent, NavbarComponent, AboutMeComponent, ExperienceComponent, ProjectsComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
